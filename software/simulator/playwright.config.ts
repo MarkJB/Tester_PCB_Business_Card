0df9f6234770f782cee91092dd4004cbcc1993b7
Mark Benson <mark@sub-design.co.uk>
2025-08-25 11:37:28 +0100

    Configure playwright html reporter

--- software/simulator/playwright.config.ts
+++ software/simulator/playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests", // or wherever your specs live inside simulator
+  testDir: "./tests",
+  reporter: [["html", { outputFolder: "test-results" }]],
 });
